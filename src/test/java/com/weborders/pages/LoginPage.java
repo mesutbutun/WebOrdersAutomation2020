64bbf4b63b256a4c0e635565c4c717254f641ca0
Mesut Butun <mstbtn@gmail.com>
2020-04-05 10:53:39 -0700

    first commit

---
+++ src/test/java/com/weborders/pages/LoginPage.java
@@ -0,0 +1,29 @@
+package com.weborders.pages;
+
+import com.weborders.utilities.ConfigurationReader;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends AbstractBasePage {
+    @FindBy(id="ctl00_MainContent_username")
+    private WebElement userName;
+    @FindBy(id="ctl00_MainContent_password")
+    private WebElement passWord;
+
+    public void login(){
+        String usernameValue= ConfigurationReader.getProperty("username");
+        String passwordValue= ConfigurationReader.getProperty("password");
+
+        userName.sendKeys(usernameValue);
+        passWord.sendKeys(passwordValue, Keys.ENTER);
+
+        public void login(String usernameValue, String passwordValue){
+            userName.sendKeys(usernameValue);
+            passWord.sendKeys(passwordValue,Keys.ENTER);
+
+        }
+
+
+    }
+}
